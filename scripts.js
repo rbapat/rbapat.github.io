713fc055c769f4b1b7500cce8e5eb2bf9604db0a
Rohan Bapat <rbapat@users.noreply.github.com>
2018-04-25 18:02:07 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $("#submit").click(function(){
         var formData = $('form').serialize();
+        var connection = new WebSocket('ws://169.254.90.228:9999');
+        connection.onopen = function () {
+            connection.send(formData); // Send the message 'Ping' to the server
+        };
         alert(formData)
     }); 
 });
