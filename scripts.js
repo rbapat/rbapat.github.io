04e703a765f01206fd72698ff2a78fb3fa94b80a
Rohan Bapat <rbapat@users.noreply.github.com>
2018-04-25 18:10:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#submit").click(function(){
         var formData = $('form').serialize();
-        var connection = new WebSocket('ws://169.254.90.228:9999');
+        var connection = new WebSocket('ws://10.100.16.241:9999');
         connection.onopen = function () {
             connection.send(formData); // Send the message 'Ping' to the server
         };
